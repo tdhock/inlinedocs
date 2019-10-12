c3ab17a8c792f7119b749d35b094633db97f9c10
Toby Dylan Hocking <tdhock5@gmail.com>
2019-10-11 20:31:43 -0700

    remove attributes docs

--- R/remove.attributes.R
+++ R/remove.attributes.R
@@ -1,4 +1,10 @@
-remove.attributes.file <- function(f){
+remove.attributes.file <- function
+### Remove attributes from all R objects defined in a file. This may
+### be useful if for some reason we want to remove the examples from
+### the ex attribute of functions.
+(f
+### File to parse and then write objects without attributes.
+){
   parsed <- parse(f)
   e <- new.env()
   eval(parsed, e)
@@ -6,6 +12,7 @@ remove.attributes.file <- function(f){
   for(obj.name in ls(e)){
     obj <- e[[obj.name]]
     attributes(obj) <- NULL
+    cat(obj.name, "<-", file=con)
     dput(obj, con)
   }
   close(con)
